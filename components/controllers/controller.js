bdb285e713e7b523cfc768be3e71177abcc9ed99
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-07-29 18:47:28 +0300

    display item on page

--- components/controllers/controller.js
+++ components/controllers/controller.js
@@ -4,38 +4,34 @@ const cheerio = require("cheerio");
 exports.send = async (req, res) => {
   const { postData } = req.body;
   const URL = `https://www.amazon.com/s?k=${postData}&ref=nb_sb_noss_1`;
-  const items = [];
+
   const itemsPage = await axios(URL)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-
-      $(".a-link-normal.s-no-outline").each((i, header) => {
-        const url = `https://www.amazon.com/${$(header).attr("href")}`;
-        items.push({
-          url,
-        });
+      const items = [];
+      $('.s-result-item'||".sg-col-4-of-12"||".sg-col-4-of-36").each((i, elem) => {
+        if ($(elem).find("img").attr("src") !== undefined) {
+          const img = $(elem).find("img").attr("src");
+          const url = `https://www.amazon.com/${$(elem)
+            .find("a.a-link-normal.s-no-outline")
+            .attr("href")}`;
+          const title = $(elem)
+            .find("span.a-size-base-plus.a-color-base.a-text-normal")
+            .text();
+          const price = $(elem).find(".a-offscreen").text()
+          console.log(price)
+          items.push({
+            title,
+            url,
+            img,
+            price,
+          });
+        }
       });
       return items;
     })
     .catch(console.error);
 
-  console.log('========',itemsPage);
+  res.status(200).send(itemsPage);
 };
-//  const getItem = async(url) => {
-//   const URL = `https://www.amazon.com/s?k=${postData}&ref=nb_sb_noss_1`;
-//   const items = [];
-//   axios(URL)
-//     .then((response) => {
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-//       $(".a-link-normal.s-no-outline").each((i, header) => {
-//         const url = `https://www.amazon.com/${$(header).attr("href")}`;
-//         items.push({
-//           url,
-//         });
-//       });
-//       return items
-//     })
-//     .catch(console.error);
-// }
