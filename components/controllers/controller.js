026d85d66e832fdd4039e76b103a35f76fe9b954
Igor Pestov <ipestov.exceedteam@gmail.com>
2020-07-31 18:16:41 +0300

    set up puppeteer

--- components/controllers/controller.js
+++ components/controllers/controller.js
@@ -1,45 +1,47 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-
+const {fetchProductList} = require("./controllerTest")
 exports.send = async (req, res) => {
   const { postData } = req.body;
-  const URL = `https://www.amazon.com/s?k=${postData}`;
-  const itemsPage = await axios(URL)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const pagesNumber = $("li.a-disabled").text()
-      console.log(pagesNumber.replace(/[^0-9\.]/g, ""));      
-      const items = [];
-      const Class =
-        ".sg-col-4-of-24.sg-col-4-of-12.sg-col-4-of-36.s-result-item.s-asin.sg-col-4-of-28.sg-col-4-of-16.sg-col.sg-col-4-of-20.sg-col-4-of-32";
-      const ClassHelp = ".s-result-item.s-asin";
-      $(ClassHelp).each((i, elem) => {
-        if ($(elem).find("img").attr("src") !== undefined) {
-          const img = $(elem).find("img").attr("src");
-          const url = `https://www.amazon.com/${$(elem)
-            .find("a.a-link-normal.s-no-outline")
-            .attr("href")}`;
-          const title = $(elem)
-            .find(".a-size-base-plus.a-color-base.a-text-normal")
-            .text();
-          const price = $(elem).find(".a-price-whole").text();
-          const priceCent = $(elem).find(".a-price-fraction").text();
-          items.push({
-            title,
-            url,
-            img,
-            price,
-            priceCent,
-          });
-          console.log(items);
-        }
-      });
+  const items = await fetchProductList('https://www.amazon.com', postData)
+//   const URL = `https://www.amazon.com/s?k=${postData}`;
+//   const items = [];
+//  await axios(URL)
+//     .then((response) => {
+//       const html = response.data;
+//       const $ = cheerio.load(html);
+//       const pagesNumber = $("li.a-disabled").text()
+//       console.log(pagesNumber.replace(/[^0-9\.]/g, ""));      
+      
+//       const Class =
+//         ".sg-col-4-of-24.sg-col-4-of-12.sg-col-4-of-36.s-result-item.s-asin.sg-col-4-of-28.sg-col-4-of-16.sg-col.sg-col-4-of-20.sg-col-4-of-32";
+//       const ClassHelp = ".s-result-item.s-asin";
+//       $(ClassHelp).each((i, elem) => {
+//         if ($(elem).find("img").attr("src") !== undefined) {
+//           const image = $(elem).find("img").attr("src");
+//           const url = `https://www.amazon.com/${$(elem)
+//             .find("a.a-link-normal.s-no-outline")
+//             .attr("href")}`;
+//           const product = $(elem)
+//             .find(".a-size-base-plus.a-color-base.a-text-normal")
+//             .text();
+//           const price = $(elem).find(".a-offscreen").text();
+//           // const priceCent = $(elem).find(".a-price-fraction").text();
+//           items.push({
+//             product,
+//             url,
+//             image,
+//             price,
+//           });
+//           console.log(items);
+//         }
+//       });
+     
 
-      return items;
-    })
-    .catch(console.error);
-  res.status(200).send(itemsPage);
+//       return items;
+//     })
+//     .catch(console.error);
+ res.status(200).send(items);
 };
 
 // const axios = require("axios");
--- components/controllers/controllerTest.js
+++ components/controllers/controllerTest.js
@@ -1,69 +1,85 @@
-const puppeteer = require('puppeteer');
-const userAgent = require('user-agents');
+const puppeteer = require("puppeteer");
+const userAgent = require("user-agents");
 
-exports.fetchProductList  = async (url, searchTerm) =>{
-    let startTime = Date.now();
-    const browser = await puppeteer.launch({ headless: true, defaultViewport: null });
-    const page = await browser.newPage();
-    await page.setUserAgent(userAgent.toString());
-    await page.goto(url, { waitUntil: 'networkidle2' });
+exports.fetchProductList = async (url, searchTerm) => {
+  let startTime = Date.now();
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+  });
+  
+  const page = await browser.newPage();
+  await page.setUserAgent(userAgent.toString());
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-    await page.waitFor('input[name="field-keywords"]');
-    await page.evaluate(val => document.querySelector('input[name="field-keywords"]').value = val, searchTerm);
+  await page.waitFor('input[name="field-keywords"]');
+  await page.evaluate(
+    (val) =>
+      (document.querySelector('input[name="field-keywords"]').value = val),
+    searchTerm
+  );
+ 
+  await page.click("div.nav-search-submit.nav-sprite");
+  await page.waitFor('div[data-cel-widget^="search_result_"]');
+  await page.waitFor('li.a-disabled') 
+  const pages = await page.evaluate(()=> {
+    let lastPages = Array.from(document.querySelectorAll('li.a-disabled'))
+    let currentPage = lastPages.map((el)=> el.innerText)
+    return Number(currentPage[1])
+  })  
 
-    await page.click('div.nav-search-submit.nav-sprite');
-
-    await page.waitFor('div[data-cel-widget^="search_result_"]');
 
     const result = await page.evaluate(() => {
-        let totalSearchResults = Array.from(document.querySelectorAll('div[data-cel-widget^="search_result_"]')).length;
-
-        let productsList = [];
-
-        for (let i = 1; i < totalSearchResults - 1; i++) {
-            let product = {
-                brand: '',
-                product: '',
-            };
-            let onlyProduct = false;
-            let emptyProductMeta = false;
-
-            let productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-base-plus.a-color-base`));
-
-            if (productNodes.length === 0) {
-                productNodes = Array.from(document.querySelectorAll(`div[data-cel-widget="search_result_${i}"] .a-size-medium.a-color-base.a-text-normal`));
-                productNodes.length > 0 ? onlyProduct = true : emptyProductMeta = true;
-            }
-
-            let productsDetails = productNodes.map(el => el.innerText);
-
-            if (!emptyProductMeta) {
-                product.brand = onlyProduct ? '' : productsDetails[0];
-                product.product = onlyProduct ? productsDetails[0] : productsDetails[1];
-            }
-
-            let rawImage = document.querySelector(`div[data-cel-widget="search_result_${i}"] .s-image`);
-            product.image =rawImage ? rawImage.src : '';
-
-            let rawUrl = document.querySelector(`div[data-cel-widget="search_result_${i}"] a.a-link-normal`);
-            product.url = rawUrl ? rawUrl.href : '';
-
-            let rawPrice = document.querySelector(`div[data-cel-widget="search_result_${i}"] span.a-offscreen`);
-            product.price = rawPrice ? rawPrice.innerText : '';
-
-            if (typeof product.product !== 'undefined') {
-                !product.product.trim() ? null : productsList = productsList.concat(product);
-            }
+      let totalSearchResults = Array.from(
+        document.querySelectorAll('div[data-cel-widget^="search_result_"]')
+      ).length;
+  
+      let productsList = [];
+  
+      for (let i = 1; i < totalSearchResults - 1; i++) {
+        let product = {
+          product: "",
+        };
+        let emptyProductMeta = false;
+  
+        let productNodes = Array.from(
+          document.querySelectorAll(
+            `div[data-cel-widget="search_result_${i}"] span.a-size-base-plus.a-color-base`
+          )
+        );
+  
+        let productsDetails = productNodes.map((el) => el.innerText);
+        if (!emptyProductMeta) {
+          product.product = productsDetails[0];
         }
-
-        return productsList;
+  
+        let rawImage = document.querySelector(
+          `div[data-cel-widget="search_result_${i}"] .s-image`
+        );
+        product.image = rawImage ? rawImage.src : "";
+  
+        let rawUrl = document.querySelector(
+          `div[data-cel-widget="search_result_${i}"] a.a-link-normal`
+        );
+        product.url = rawUrl ? rawUrl.href : "";
+  
+        let rawPrice = document.querySelector(
+          `div[data-cel-widget="search_result_${i}"] span.a-offscreen`
+        );
+        product.price = rawPrice ? rawPrice.innerText : "";
+  
+        if (typeof product.product !== "undefined") {
+          !product.product.trim()
+            ? null
+            : (productsList = productsList.concat(product));
+        }
+      }
+  
+      return productsList;
     });
-
-    console.log(result);
-
-    await browser.close();
-
-    console.log('Time: ', (Date.now() - startTime) / 1000, 's');
-}
-
-fetchProductList('https://www.amazon.in', 'Shirts');
+    
+  
+  await browser.close();
+  console.log("Time: ", (Date.now() - startTime) / 1000, "s");
+  return result;
+};
